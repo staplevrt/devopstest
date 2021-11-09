938fb6b72c4acdd851cc0c8b4d1a38439247d29a
Bryan <kangah_b@etna-alternance.net>
2021-11-09 16:37:55 +0100

    first try

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world IIM students!
+          Hello world IIM student!
         </p>
         <a
           className="App-link"
